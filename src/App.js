6f8a0960d4a44f7246d44a793138fc3635d18942
Amr_Yasser <amr.yasser@intellisc.com>
2022-07-20 16:31:41 +0200

    adding the total and amount calc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Navbar from "./components/Navbar";
 import CardContainer from "./components/CardContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 const App = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
